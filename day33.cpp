b51e177809be30d2ae4afecc3718084ec962f0d8
kartikmadan26 <72307166+kartikmadan26@users.noreply.github.com>
2024-02-27 10:32:34 +0530

    Create day33.cpp

---
+++ day33.cpp
@@ -0,0 +1,28 @@
+// Question
+// 100. Same Tree
+// Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+// Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+// Example 1:
+// Input: p = [1,2,3], q = [1,2,3]
+// Output: true
+
+// Example 2:
+// Input: p = [1,2], q = [1,null,2]
+// Output: false
+
+// Example 3:
+// Input: p = [1,2,1], q = [1,1,2]
+// Output: false
+ 
+// Constraints:
+// The number of nodes in both trees is in the range [0, 100].
+// -104 <= Node.val <= 104
+// Solution
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (p == NULL || q == NULL) return (p == q);
+        return (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+    }
+};
